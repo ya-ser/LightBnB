bdb11b94570f81a0479b47945e1765175ba9b6a9
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-01-06 23:44:07 +0000

    update getUserWithEmail, getUserWithId, and addUser with query code

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,18 +17,14 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
-}
+ const getUserWithEmail = (email) => {
+  return pool
+    .query(`SELECT email FROM users WHERE email LIKE $1;`, [`%${email}%`])
+    .then((result) => result.rows[0] || null)
+    .catch((err) => {
+      console.log(err.message);
+  });
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,9 +32,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
-}
+ const getUserWithId = (id) => {
+  return pool
+    .query(`SELECT id FROM users WHERE id = $1;`, [`${id}`])
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+  });
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -47,12 +48,14 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-}
+ const addUser = (user) => {
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [`${user.name}`, `${user.email}`, `${user.password}`])
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+  });
+};
 exports.addUser = addUser;
 
 /// Reservations
