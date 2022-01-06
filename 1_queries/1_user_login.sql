cbbeee675e65e09ca9805d5d0c64ecbbfeaf99b6
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-01-06 22:33:47 +0000

    add lightbnb zip to directory

--- 1_queries/1_user_login.sql
+++ 1_queries/1_user_login.sql
@@ -1,4 +1,5 @@
 -- Select their id, name, email, and password.
 -- Select a single user using their email address. Use tristanjacobs@gmail.com for now.
-SELECT *
-FROM property_reviews;
+SELECT id, name, email, password
+FROM users
+WHERE email = 'ariaatkinson@outlook.com';
